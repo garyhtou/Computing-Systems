05bff969bcda6908a45232282a4cffc2a468b1f3
Gary Tou <gary@garytou.com>
2022-01-10 20:07:14 -0800

    WIP wgrep

---
+++ project1/wgrep.cpp
@@ -0,0 +1,76 @@
+// Gary Tou
+// January 10, 2022
+// CPSC 3500, Project 1
+// wgrep.cpp
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void grepStream(istream &stream, char *searchTerm)
+{
+	// Assume the stream is open (in the case of a file)
+
+	string line;
+	// Loop through all lines in the stream
+	while (getline(stream, line))
+	{
+		// Search the string (line) for the search term
+		if (line.find(searchTerm) != string::npos)
+		{
+			// The string contains the search term. Print the line
+			cout << line << endl;
+		}
+	}
+}
+
+int main(int arg, char *argv[])
+{
+	// Guard case for when no command-line arguments are passed in.
+	// This guarrantees that we have a search term argument (however, there may
+	// still be no file paths)
+	if (arg <= 1)
+	{
+		cout << "wcat: file [file ...]" << endl;
+		return 1;
+	}
+
+	char *searchTerm = argv[1];
+
+	// Guard case for when no file paths are passed in
+	if (arg <= 2)
+	{
+		// We have a search term, but no file paths. Read the file from the standard
+		// input (stdin)
+		grepStream(stdin, searchTerm);
+
+		// we're done!
+		return 0;
+	}
+
+	// Iterate through the command-line arguments (file paths).
+	// The first file path argument starts at index 2 since argv[0] is the name of
+	// the program (wgrep) and argv[1] is the search term
+	for (int i = 2; i < arg; i++)
+	{
+		// Get the file path
+		char *filePath = argv[i];
+
+		// Create a file stream
+		ifstream file(filePath);
+
+		// Guard case for when the file stream fails to open
+		if (!file.is_open())
+		{
+			// Since the file stream failed to open, print an error message
+			cout << "wgrep: cannot open file" << endl;
+			exit(1);
+		}
+
+		// Grep the file stream
+		grepStream(file, searchTerm);
+	}
+
+	// We've grep'ed all the files
+	return 0;
+}
