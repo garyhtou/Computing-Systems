01e6852b7ae76f5676b45f71a4382eb2addf514f
Gary Tou <gary@garytou.com>
2022-01-10 19:25:55 -0800

    Project 1: Makefile + wcat

---
+++ project1/wcat.cpp
@@ -0,0 +1,47 @@
+// Gary Tou
+// January 10, 2022
+// CPSC 3500, Project 1
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void printFile(char *filePath)
+{
+	// Create a file stream with the provided file path
+	ifstream file(filePath);
+
+	// Check if the file stream is open
+	if (file.is_open())
+	{
+		// print out the stream buffer
+		cout << file.rdbuf();
+	}
+	else
+	{
+		// Since the file stream failed to open, print an error message
+		cout << "wcat: cannot open file" << endl;
+		exit(1);
+	}
+}
+
+int main(int arg, char *argv[])
+{
+	// Guard case for when no command-line arguments are passed in
+	if (arg == 0)
+	{
+		cout << "wcat: file [file ...]" << endl;
+		return 1;
+	}
+
+	// Iterate through the command-line arguments (file paths)
+	for (int i = 0; i < arg - 1; i++)
+	{
+		// Print the file.
+		//   The first command-line arguments starts at index 1 since argv[0] is the
+		//   name of the program (wcat)
+		printFile(argv[i + 1]);
+	}
+
+	return 0;
+}
